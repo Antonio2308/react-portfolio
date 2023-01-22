b49db03fea60ddd9413e4420692c24e44a66fea9
Antonio2308 <64449887+Antonio2308@users.noreply.github.com>
2023-01-22 01:11:04 -0400

    update config

--- next.config.js
+++ next.config.js
@@ -4,3 +4,18 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+const withCSS = require('@zeit/next-css')
+const withPlugins = require('next-compose-plugins')
+const withTM = require('next-transpile-modules')(['tailwindcss'])
+
+module.exports = withPlugins([withCSS, withTM], {
+  postcssLoaderOptions: {
+    parser: 'postcss-scss',
+    config: {
+      ctx: {
+        tailwind: './tailwind.config.js'
+      }
+    }
+  }
+})
